19dbb92e6ca05920b6cb86ba3e6f5e8da0872e17
manojkumar-ch <42571448+manojkumar-ch@users.noreply.github.com>
2019-02-20 15:02:49 +0530

    Delete kir12.py

--- kir12.py
+++
@@ -1,9 +0,0 @@
-b=int(input('Enter the number to check: \n'))
-rank=0
-for i in range (2,b):
-	if(b%i==0):
-		rank = rank+1
-if(rank==0):
-	print('Yes, It is a prime number')
-else:
-	print('No, It is not a prime number')
